1415ff150611e885eca405d8f0306a6cfe8863bf
Brunoxd23 <monteiro_100@outlook.pt>
2026-04-29 19:41:09 -0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+function Header(props) {
+  // eslint-disable-next-line react/prop-types
+  return <header className="header">{props.children}</header>;
+}
+
+export default Header;
---
+++ src/components/Task.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/jsx-key */
+import { useState } from "react";
+import Header from "./Header";
+
+function Task() {
+  const [inputValue, setInputValue] = useState("Teste");
+  const [messages, setMessages] = useState([
+    "Hello World",
+    "Welcome to the Task Manager",
+  ]);
+
+  function handleButtonClick() {
+    setMessages([...messages, inputValue]);
+  }
+
+  return (
+    <div>
+      <Header></Header>
+      <input
+        type="text"
+        placeholder="Create your task..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleButtonClick}>Add Message</button>
+      <ul>
+        {messages.map((message) => {
+          return <li>{message}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Task;
