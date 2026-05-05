e001f280c8ab03196696614359fa49df134e0e8c
Brunoxd23 <monteiro_100@outlook.pt>
2026-05-04 22:08:53 -0300

    chore: add assets barrel export

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import TrashIcon from "../assets/icons/trash.svg?react"
+import AddIcon from "../assets/icons/add.svg?react"
+import SunIcon from "../assets/icons/sun.svg?react"
+import MoonIcon from "../assets/icons/moon.svg?react"
+import CloudIcon from "../assets/icons/cloud-sun.svg?react"
+
+export { TrashIcon, AddIcon, SunIcon, MoonIcon, CloudIcon }
